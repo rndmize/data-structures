a7b16b7ab1f1def21ea25a2f8e22121a29c69dc6
Hack Reactor Students <rndmize@gmail.com>
2014-02-27 16:33:09 -0800

    Passes ALL tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,23 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this._storage = {};
+  this._start = 0;
+  this._end = 0;
+};
+
+Queue.prototype.enqueue = function(item){
+  this._storage[this._end] = item;
+  this._end++;
+};
+
+Queue.prototype.dequeue = function(){
+  if(this._start === this._end){
+    return console.log('Y U DO DIS');
+  }
+  var item = this._storage[this._start];
+  this._start++;
+  return item;
+};
+
+Queue.prototype.size = function(){
+  return this._end - this._start;
 };
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,25 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+
+  this._storage = {};
+  this._length = 0; // Hint: set an initial value here
 };
+
+Stack.prototype.size = function() {
+  return this._length;
+};
+
+Stack.prototype.pop = function() {
+  if (this._length <= 0) {
+    return console.log('Empty stack');
+  }
+  this._length--;
+  var item = this._storage[this._length];
+  return item;
+};
+
+Stack.prototype.push = function(item) {
+  this._storage[this._length] = item;
+  this._length++;
+};
+
+
